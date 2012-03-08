b242dc58f26f9d3aea6ac5bd116f2e575f04c6a5
Peter J. Holzer <hjp@hjp.at>
2012-03-08 22:25:50 +0100

    Some variants of the Sieve of Eratosthenes in Perl.
    Surprisingly, the naive implementation is the fastest.

---
+++ eratosthenes-str.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Time::HiRes qw(time);
+
+my $t0 = time;
+
+my $n = 1_000_000;
+
+my $sieve = "0" x $n;
+
+for my $i (2 .. $n) {
+    next if substr($sieve, $i-1, 1);
+    print "$i\n";
+    my $k = $n / $i;
+    my $mask = ("0" x ($i - 1) . "1") x $k;
+    $sieve |= $mask;
+}
+
+my $t1 = time;
+print $t1 - $t0, "\n";
+
---
+++ eratosthenes-vec.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Time::HiRes qw(time);
+
+my $t0 = time;
+
+my $n = 1_000_000;
+
+my $sieve = "";
+
+for my $i (2 .. $n) {
+    next if vec($sieve, $i, 1);
+    print "$i\n";
+    for (my $j = 2 * $i; $j <= $n; $j += $i) {
+	vec($sieve, $j, 1) = 1;
+    }
+}
+
+my $t1 = time;
+print $t1 - $t0, "\n";
