124068f6d5c509e0730f94bd92af53f4853818bc
Peter J. Holzer <hjp@hjp.at>
2014-12-17 23:05:29 +0100

    A variant of the C example using double instead of int.

---
+++ C_double/eratosthenes.c
@@ -0,0 +1,34 @@
+/* Sieve of eratosthenes in C
+ *
+ * This differs from the example in directory ../C by doing all the
+ * arithmetic in double instead of int - just to check how much slower
+ * it is (on a Core2 Duo using gcc 4.9.1 for i586-linux-gnu about 5
+ * times slower)
+ */
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+#define N 1000000
+
+int main(void) {
+    char sieve[N+1];
+    struct timeval t0, t1;
+
+    gettimeofday(&t0, NULL);
+
+    for (double i = 2; i <= N; i++) {
+        if (sieve[(int)i]) continue;
+
+        // printf("%d\n", i);
+        for (double j = 2 * i; j <= N; j += i) {
+            sieve[(int)j] = 1;
+        }
+    }
+
+    gettimeofday(&t1, NULL);
+
+    printf("%g\n",
+           ((t1.tv_sec - t0.tv_sec) * 1E6 + (t1.tv_usec - t0.tv_usec)) / 1E6);
+    return 0;
+}
